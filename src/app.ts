3caaebefaf29a1e09c9f8f7c6a9f954ecc8ba877
azu <azuciao@gmail.com>
2022-11-11 21:44:54 +0900

    add lintGutter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { basicSetup, EditorView } from "codemirror";
 import { secretlint } from "./secretlint";
 import { DEFAUT_TEXT } from "./DEFAUT_TEXT";
+import { lintGutter } from "@codemirror/lint";
 
 const editorElement = document.getElementById("editor")!;
 new EditorView({
     doc: DEFAUT_TEXT,
-    extensions: [basicSetup, secretlint],
+    extensions: [basicSetup, lintGutter(), secretlint],
     parent: editorElement
 });
 
