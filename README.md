2c9aafa7e9dbfd75c00c4b35d99c5110644d30e7
azu <azuciao@gmail.com>
2022-11-11 21:33:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# secretlint.github.io
+
+Secretint Demo Site
+
+## Usage
+
+- Visit: https://secretlint.github.io/
+
+## Development
+
+    npm install
+    npm run dev ite
+
+## Contributing
+
+Pull requests and stars are always welcome.
+
+For bugs and feature requests, [please create an issue](https://github.com/secretlint/secretlint.github.io/issues).
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Author
+
+- azu: [GitHub](https://github.com/azu), [Twitter](https://twitter.com/azu_re)
+
+## License
+
+MIT © azu
---
+++ src/secretlint.ts
@@ -0,0 +1,50 @@
+import { lintSource } from "@secretlint/core";
+// @ts-expect-error: no types
+import { creator } from "@secretlint/secretlint-rule-preset-recommend";
+import { Diagnostic, linter } from "@codemirror/lint";
+
+// CodeMirror Linter implemetnion
+export const secretlint = linter(async (view) => {
+    const results = await lintContent({
+        content: view.state.doc.toString(),
+        url: "example.txt"
+    });
+    const diagnostics: Diagnostic[] = results.messages.map((message) => {
+        return {
+            from: message.range[0],
+            to: message.range[1],
+            message: message.message,
+            severity: message.severity,
+            actions: [
+                {
+                    name: "Documentation",
+                    apply: () => {
+                        window.open(`${message.docsUrl}#${message.messageId}`, "_blank");
+                    }
+                }
+            ]
+        };
+    });
+    return diagnostics;
+});
+export const lintContent = ({ content, url }: { content: string; url: string }) => {
+    const rules = [
+        {
+            id: "@secretlint/secretlint-rule-preset-recommend",
+            rule: creator
+        }
+    ];
+    return lintSource({
+        source: {
+            contentType: "text",
+            content,
+            filePath: url,
+            ext: url.split(".").pop() ?? ""
+        },
+        options: {
+            config: {
+                rules: rules
+            }
+        }
+    });
+};
